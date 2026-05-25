40917fadb19813b74d0246ba3c40715938f2dbaa
Deepanshu Kumar <deepanshuk1710@gmail.com>
2026-05-25 11:40:41 +0000

    add basic tcp server with ping response

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,73 @@
+#include <arpa/inet.h>
+#include <cstring>
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include <sstream>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main()
 {
-  std::unordered_map<std::string, std::string> store;
+  const int port = 6379;
 
-  std::cout << "RedisLiteX fake server started. Type commands.\n";
-  std::cout << "Available commands: PING, SET key value, GET key, DEL key, EXIT\n";
-
-  std::string line;
-
-  while (true)
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1)
   {
-    std::cout << "> ";
-    std::getline(std::cin, line);
-
-    std::stringstream ss(line);
-    std::string command;
-    ss >> command;
-
-    if (command == "PING")
-    {
-      std::cout << "PONG\n";
-    }
-    else if (command == "SET")
-    {
-      std::string key, value;
-      ss >> key >> value;
-
-      if (key.empty() || value.empty())
-      {
-        std::cout << "ERR usage: SET key value\n";
-      }
-      else
-      {
-        store[key] = value;
-        std::cout << "OK\n";
-      }
-    }
-    else if (command == "GET")
-    {
-      std::string key;
-      ss >> key;
-
-      if (store.find(key) == store.end())
-      {
-        std::cout << "(nil)\n";
-      }
-      else
-      {
-        std::cout << store[key] << "\n";
-      }
-    }
-    else if (command == "DEL")
-    {
-      std::string key;
-      ss >> key;
-
-      int deleted = store.erase(key);
-      std::cout << deleted << "\n";
-    }
-    else if (command == "EXIT")
-    {
-      std::cout << "Bye\n";
-      break;
-    }
-    else
-    {
-      std::cout << "ERR unknown command\n";
-    }
+    std::cerr << "Could not create server socket\n";
+    return 1;
   }
 
+  int reuse = 1;
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+  sockaddr_in server_addr{};
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(port);
+
+  if (bind(server_fd, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) == -1)
+  {
+    std::cerr << "Could not bind to port " << port << "\n";
+    close(server_fd);
+    return 1;
+  }
+
+  if (listen(server_fd, 5) == -1)
+  {
+    std::cerr << "Could not listen for connections\n";
+    close(server_fd);
+    return 1;
+  }
+
+  std::cout << "RedisLiteX listening on port " << port << "\n";
+
+  sockaddr_in client_addr{};
+  socklen_t client_len = sizeof(client_addr);
+
+  int client_fd = accept(server_fd, reinterpret_cast<sockaddr *>(&client_addr), &client_len);
+  if (client_fd == -1)
+  {
+    std::cerr << "Could not accept client\n";
+    close(server_fd);
+    return 1;
+  }
+
+  std::cout << "Client connected\n";
+
+  char buffer[1024]{};
+  ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+  if (bytes_read > 0)
+  {
+    std::string request(buffer, bytes_read);
+    std::cout << "Received: " << request << "\n";
+
+    std::string response = "+PONG\r\n";
+    send(client_fd, response.c_str(), response.size(), 0);
+  }
+
+  close(client_fd);
+  close(server_fd);
+
+  std::cout << "Server stopped\n";
   return 0;
 }
